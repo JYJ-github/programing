705b48b63ccc1efdf2751cfcde665afa6cca2b08
eidoj <117913955+choyoungj@users.noreply.github.com>
2022-11-15 15:43:05 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    my_string = my_string.replace(/a/g,'')
+    my_string = my_string.replace(/i/g,'')
+    my_string = my_string.replace(/e/g,'')
+    my_string = my_string.replace(/o/g,'')
+    my_string = my_string.replace(/u/g,'')
+
+    return my_string
+}
