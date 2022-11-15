6e399970cfd7af7467a6c24e7c2cd5783f13c4c3
eidoj <117913955+choyoungj@users.noreply.github.com>
2022-11-15 20:03:20 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,6 +1,14 @@
 function solution(price) {
-  if (price >= 500000) return Math.floor(price * 0.8);
-  if (price >= 300000) return Math.floor(price * 0.9);
-  if (price >= 100000) return Math.floor(price * 0.95);
-  return price;
+  if (price >= 500000) {
+      return Math.floor(price * 0.8);
+  }
+  else if (price >= 300000) {
+      return Math.floor(price * 0.9);
+  }
+  else if (price >= 100000) {
+      return Math.floor(price * 0.95);
+  } else {
+      return price;
+  }
+  
 }
