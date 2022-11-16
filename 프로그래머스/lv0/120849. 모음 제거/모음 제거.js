3688da5c5a3b6d5e8a1aa29323bfa1846e4d014e
eidoj <117913955+choyoungj@users.noreply.github.com>
2022-11-16 19:21:15 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,9 +1,8 @@
 function solution(my_string) {
-    my_string = my_string.replace(/a/g,'')
-    my_string = my_string.replace(/i/g,'')
-    my_string = my_string.replace(/e/g,'')
-    my_string = my_string.replace(/o/g,'')
-    my_string = my_string.replace(/u/g,'')
-
+    let str_arr = ['a', 'i', 'u', 'e', 'o']
+    for (let i = 0; i < str_arr.length; i++) {
+        let pattern = new RegExp(str_arr[i], "g")
+        my_string = my_string.replace(pattern, '')
+    }
     return my_string
 }
