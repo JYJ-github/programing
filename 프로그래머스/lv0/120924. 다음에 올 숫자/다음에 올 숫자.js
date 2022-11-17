e22cc66387b289151700e857d91f39e6e5319d7f
eidoj <117913955+choyoungj@users.noreply.github.com>
2022-11-18 00:27:14 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,7 @@
+function solution(common) {
+    if (common[0] - common[1] === common[1] - common[2]) {
+        return common[common.length - 1] + common[1] - common[0]
+    } else {
+        return common[common.length - 1] * (common[1] / common[0])
+    }
+}
