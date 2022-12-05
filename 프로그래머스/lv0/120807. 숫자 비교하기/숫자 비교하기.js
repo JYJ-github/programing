364e27774901b223760d3ca7db661eff9f7aa386
eidoj <117913955+choyoungj@users.noreply.github.com>
2022-12-05 21:46:50 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,8 +1,2 @@
-function solution(num1, num2) {
-    if (num1 === num2) {
-        return 1
-    } else {
-        return -1
-    }
-        
-}
+//화살표함수와 삼항연산자 활용
+const solution = (num1, num2) => (num1 === num2 ? 1 : -1);
